9f513c57bb782b682257d184ff288e187bd36083
Hajime Hoshi <hajimehoshi@gmail.com>
2020-09-28 17:42:21 +0900

    Update the license comment

--- font.go
+++ font.go
@@ -17,7 +17,7 @@
 //   * [Baekmuk Gulim](https://kldp.net/baekmuk/) (Baekmuk License)
 //   * [misc-fixed](https://www.cl.cam.ac.uk/~mgk25/ucs-fonts.html) (Public Domain)
 //   * [M+ Bitmap Font](https://mplus-fonts.osdn.jp/mplus-bitmap-fonts/) (M+ Bitmap Fonts License)
-//   * Arabic glyphs by [@MansourSorosoro](https://twitter.com/MansourSorosoro) (Eternal Dream Arabization) (Apatche License 2.0)
+//   * Arabic glyphs by [@MansourSorosoro](https://twitter.com/MansourSorosoro) (Eternal Dream Arabization) (OFL-1.1)
 package bitmapfont
 
 import (
