0e15abf1fbafc681a0d62c544ebcb29cb967cd41
Hajime Hoshi <hajimehoshi@gmail.com>
2024-07-04 12:13:09 +0900

    bitmapfont: remove a duplicated comment

--- lazyface.go
+++ lazyface.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package bitmapfont provides font.Face values with bitmap glyphs.
 package bitmapfont
 
 import (
