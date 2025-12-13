235ee36224352accf0701194285395f541282d89
Hajime Hoshi <hajimehoshi@gmail.com>
2025-12-13 13:58:22 +0900

    internal/ark: update Ark to 2025.12.11

--- internal/ark/glyph.go
+++ internal/ark/glyph.go
@@ -24,7 +24,7 @@ import (
 )
 
 // The current version is:
-// https://github.com/TakWolf/ark-pixel-font/releases/tag/2025.10.20
+// https://github.com/TakWolf/ark-pixel-font/releases/tag/2025.12.11
 // (ark-pixel-font-12px-monospaced-bdf-*.zip)
 
 func readBDF(filename string) (map[rune]*bdf.Glyph, error) {
